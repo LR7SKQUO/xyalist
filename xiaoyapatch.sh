fda554ee71d18fb129a4bc689c38518abfa6c92b
LR7SKQUO <4255110+LR7SKQUO@users.noreply.github.com>
2023-10-06 09:03:01 +0800

    Update xiaoyapatch.sh
    remove nginx conf (emby) proxy cache

--- xiaoyapatch.sh
+++ xiaoyapatch.sh
@@ -18,6 +18,27 @@ sed -i "s/$sedSearch//g" /var/lib/aaa/nginx.conf
 sedSearch=$(sed_string_escape 'proxy_cache apicache;')
 sed -i "s/$sedSearch//g" /var/lib/aaa/nginx.conf
 
+sedSearch=$(sed_string_escape 'proxy_cache_path /var/cache/nginx/emby levels=1:2 keys_zone=emby:100m max_size=1g inactive=30d use_temp_path=off;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_path /var/cache/nginx/emby/subs levels=1:2 keys_zone=embysubs:10m max_size=1g inactive=30d use_temp_path=off;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache embysubs;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_revalidate on;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_lock_timeout 10s;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_lock on;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_valid 200 30d;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache_key $proxy_host$uri;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'add_header X-Cache-Status $upstream_cache_status;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+sedSearch=$(sed_string_escape 'proxy_cache emby;')
+sed -i "s/$sedSearch//g" /var/lib/aaa/emby.conf
+
 cd /var/lib/aaa
 zip -q -r html.zip *
 mv html.zip /var/lib/data.zip
